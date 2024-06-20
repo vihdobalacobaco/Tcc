300cfb9e90751b92e8375e852cb935ff5742a59f
Vitoria <vitoriajemima2007@gmail.com>
2024-06-19 22:24:54 -0300

    Tela de login atualizada

--- app/src/main/java/com/example/pdmo/navegador/navegadorweb/tccprojeto1/MainActivity.java
+++ app/src/main/java/com/example/pdmo/navegador/navegadorweb/tccprojeto1/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.pdmo.navegador.navegadorweb.tccprojeto1;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.Window;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ActionBar actionBar = getSupportActionBar();
+        Window window = getWindow();
+        window.setStatusBarColor(Color.parseColor("#FFFFFF"));
     }
 }
